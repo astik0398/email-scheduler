da58b00f76038ce63e2a8fbef19cb32088ad2100
Kumar Astik <kumarastik0398@gmail.com>
2024-01-05 02:03:11 +0530

    mock 3 nykaa backend deployment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const {cors} = require('cors')
+// const {cors} = require('cors')
 const app = express()
 require('dotenv').config()
 app.use(express())
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
 const {connection} = require('./db')
 const {productRouter} = require('./routes/product.router')
